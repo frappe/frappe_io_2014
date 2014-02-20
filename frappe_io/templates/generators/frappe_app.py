39a86031a42fb4c1a891c8b35a97f039221fcdd3
Anand Doshi <anand@erpnext.com>
2014-02-20 18:19:52 +0530

    Apps, App Testimonial, Styling

--- frappe_io/templates/generators/frappe_app.py
+++ frappe_io/templates/generators/frappe_app.py
@@ -1,2 +1,18 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+
+import frappe
+from frappe.website.doctype.website_slideshow.website_slideshow import get_slideshow
+
 doctype = "Frappe App"
 condition_field = "published"
+
+def get_context(context):
+	app_context = {}
+	if context.bean.doc.slideshow:
+		app_context["slideshow"] = context.bean.doc.slideshow
+		app_context.update(get_slideshow(context.bean))
+	
+	return app_context
