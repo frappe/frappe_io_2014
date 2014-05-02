9edf588e213f8c293bd7442e51f2a5fcb73fec37
Rushabh Mehta <rmehta@gmail.com>
2014-05-02 12:14:06 +0530

    hooks.txt -> hooks.py

---
+++ frappe_io/hooks.py
@@ -0,0 +1,24 @@
+app_name = "frappe_io"
+app_title = "Frappe.io Portal"
+app_publisher = "Web Notes Technologies Pvt Ltd"
+app_description = "Community portal for Frappe"
+app_icon = "assets/frappe/images/frappe.svg"
+app_color = "#888"
+app_email = "info@erpnext.com"
+app_url = "http://frappe.io"
+app_version = "0.0.1"
+
+home_page = "index"
+brand_html = '<img src="/assets/frappe_io/images/frappe-io-brand.png" style="width: 30px;" /> Frappe.io <span class="text-muted" style="font-size: 9px; margin-left: -20px; margin-top: 15px; display: inline-block;position: absolute; font-weight: normal;">Beta</span>'
+
+web_include_css = "assets/frappe_io/css/frappe-io-web.css"
+
+fixtures = [
+	"Website Settings",
+	"Website Script",
+	"Contact Us Settings",
+	"Frappe Publisher",
+	"Frappe App",
+	"Website Group",
+	"Frappe Partner"
+]
