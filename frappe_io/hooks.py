1287e1090bcd85ab01edcd368691002a027501fe
Anand Doshi <anand@erpnext.com>
2014-05-05 18:35:25 +0530

    hide_in_installer = True

--- frappe_io/hooks.py
+++ frappe_io/hooks.py
@@ -7,6 +7,7 @@ app_color = "#888"
 app_email = "info@erpnext.com"
 app_url = "http://frappe.io"
 app_version = "0.0.1"
+hide_in_installer = True
 
 home_page = "home"
 brand_html = '<img src="/assets/frappe/images/frappe.svg" style="width: 30px;" /> Frappe.io <span class="text-muted" style="font-size: 9px; margin-left: -20px; margin-top: 15px; display: inline-block;position: absolute; font-weight: normal;">Beta</span>'
