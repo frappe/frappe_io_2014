5668792bb65199d8fe382821e781b2c3866e3036
Anand Doshi <anand@erpnext.com>
2014-05-16 23:32:51 +0530

    Fixed config/desktop.py

--- frappe_io/config/desktop.py
+++ frappe_io/config/desktop.py
@@ -1,10 +1,11 @@
 from frappe import _
 
-data = {
-	"Frappe Apps": {
-		"color": "orange",
-		"icon": "assets/frappe/images/frappe.svg",
-		"label": _("Frappe.io Portal"),
-		"type": "module"
+def get_data():
+	return {
+		"Frappe Apps": {
+			"color": "orange",
+			"icon": "assets/frappe/images/frappe.svg",
+			"label": _("Frappe.io Portal"),
+			"type": "module"
+		}
 	}
-}
