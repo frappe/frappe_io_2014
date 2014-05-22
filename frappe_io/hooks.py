51a818f3746b67fa95098ea277a5f99b90fd6a42
Rushabh Mehta <rmehta@gmail.com>
2014-05-22 19:48:22 +0530

    style

--- frappe_io/hooks.py
+++ frappe_io/hooks.py
@@ -10,7 +10,7 @@ app_version = "0.0.1"
 hide_in_installer = True
 
 home_page = "home"
-brand_html = '<img src="/assets/frappe/images/frappe.svg" style="width: 30px;" /> Frappe.io <span class="text-muted" style="font-size: 9px; margin-left: -20px; margin-top: 15px; display: inline-block;position: absolute; font-weight: normal;">Beta</span>'
+brand_html = '<img src="/assets/frappe_io/images/frappe-io-brand.png" style="width: 30px;" /> Frappe.io <span class="text-muted" style="font-size: 9px; margin-left: -20px; margin-top: 15px; display: inline-block;position: absolute; font-weight: normal;">Beta</span>'
 
 web_include_css = "assets/frappe_io/css/frappe-io-web.css"
 
